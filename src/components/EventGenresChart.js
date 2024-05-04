01998a224d8be3c91cba4ee9dae4373deb566e82
Priyankareddy-nalla <148527465+Priyankareddy-nalla@users.noreply.github.com>
2024-05-04 21:32:36 +0200

    Adjusted Pie chart size

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -52,7 +52,7 @@ const EventGenresChart = ({ events }) => {
                     fill="#8884d8"
                     labelLine={false}
                     label={renderCustomizedLabel}
-                    outerRadius={150}
+                    outerRadius={130}
                 >
                     {data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
